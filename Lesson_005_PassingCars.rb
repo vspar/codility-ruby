5fc9480f44d9ae7c1ec06943ec082ded86ac6404
Vic Spanner <vic@spannersoftware.com>
2020-01-21 10:00:19 +0000

    Updated Lesson 5, PassingCars (better solution)

--- Lesson_005_PassingCars.rb
+++ Lesson_005_PassingCars.rb
@@ -1,23 +1,40 @@
 class PassingCars
 
   def self.solution(a)
-    result = 0
-    len = a.size
-    passing = a.reduce(:+)
-    passed = 0
+    passing = 0
+    going_west = 0
 
-    (0..len-1).each do |i|
+    (a.size-1).downto(0) do |i|
       if a[i] == 0
-        result += passing - passed
+        passing += going_west
       else
-        passed += 1
+        going_west += 1
       end
 
-      return -1 if result > 1000000000
+      return -1 if passing > 1000000000
     end
 
-    return result
+    return passing
   end
+
+  #def self.solution(a)
+  #  result = 0
+  #  len = a.size
+  #  passing = a.reduce(:+)
+  #  passed = 0
+  #
+  #  (0..len-1).each do |i|
+  #    if a[i] == 0
+  #      result += passing - passed
+  #    else
+  #      passed += 1
+  #    end
+  #
+  #    return -1 if result > 1000000000
+  #  end
+  #
+  #  return result
+  #end
 
 end
 
@@ -33,5 +50,5 @@ puts "#{result}, pass: #{result == 0}"
 result = PassingCars.solution([0, 1, 0, 1, 0, 0, 1, 1, 0, 0])
 puts "#{result}, pass: #{result == 11}"
 
-# TESTED AND PASSED ON Codility
+# TESTED AND PASSED ON Codility (both solutions)
 
